9abca1677921bb37a37efb2c7c0fe01619cecd2b
Alec Hannaford <alec322@gmail.com>
2023-08-23 21:23:53 -0700

    skip issue due to float rounding error

--- conversions.js
+++ conversions.js
@@ -1,5 +1,18 @@
-let values = {}
+function oneDecRound(num){
+    return Math.round(num * 10) / 10
+}
+
+let mg_dL_to_mmol_L = {}
 
 for (let i = 0; i <= 600; i++){
-    
+    mg_dL_to_mmol_L[i] = oneDecRound(i/18.018)
 }
+
+let mmol_L_to_mg_dL = {}
+
+for (let i = 0.0; i < 33.4; i += 0.1){
+    mmol_L_to_mg_dL[oneDecRound(i)] = oneDecRound(oneDecRound(i) * 18.018)
+}
+
+//console.log(mg_dL_to_mmol_L)
+console.log(mmol_L_to_mg_dL)
